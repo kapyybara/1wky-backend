24b24ed4fb3fc06ae81c3b3d7974c9f4d737d9ac
Phạm Nhật Tiến <83960491+1wku@users.noreply.github.com>
2022-01-30 23:23:11 +0700

    fix bug

--- client/src/apis/axiosClient.js
+++ client/src/apis/axiosClient.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 import queryString from 'query-string'
 
 export default async function request(url, method, body, params) {
-	const baseURL = process.env.REACT_APP_API_URL || 'https://tinevy.herokuapp.com/'
+	const baseURL = process.env.REACT_APP_API_URL || 'https://tinevy.herokuapp.com'
 	const headers = {
 		'Content-Type': 'application/json',
 		'Access-Control-Allow-Origin': '*',
