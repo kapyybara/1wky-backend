d8ea18ee0f825ff0c0be37a6c06512815cde65c6
Phạm Nhật Tiến <83960491+1wku@users.noreply.github.com>
2022-01-31 00:17:35 +0700

    fix bug

--- client/src/apis/axiosClient.js
+++ client/src/apis/axiosClient.js
@@ -2,7 +2,6 @@ import axios from 'axios'
 import queryString from 'query-string'
 
 export default async function request(url, method, body, params) {
-	const baseURL = process.env.REACT_APP_API_URL || 'https://tinevy.herokuapp.com'
 	const headers = {
 		'Content-Type': 'application/json',
 		'Access-Control-Allow-Origin': '*',
@@ -11,7 +10,7 @@ export default async function request(url, method, body, params) {
 
 	let objMeta = {
 		method,
-		url: `${baseURL}${url}${`/?${queryFormarted}` || ''}`,
+		url: `${url}${`/?${queryFormarted}` || ''}`,
 		headers,
 		data: body,
 	}
